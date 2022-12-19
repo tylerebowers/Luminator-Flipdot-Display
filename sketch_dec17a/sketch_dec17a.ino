a263cf2d65b40a3222f7efb3fd876e7ab997be03
Tyler Bowers <tylerebowers@gmail.com>
2022-12-18 19:14:57 -0500

    up

--- sketch_dec17a/sketch_dec17a.ino
+++ sketch_dec17a/sketch_dec17a.ino
@@ -6,11 +6,11 @@ struct shiftRegister{
   int clear;
 } col, row;
 
-void writeRegister(struct shiftRegister sr, int array[16]){
+void writeRegister(struct shiftRegister sr, int array[8]){
   digitalWrite(sr.outputEnable, HIGH);
   digitalWrite(sr.clear, LOW);
   digitalWrite(sr.clear, HIGH);
-  for(int i = 0; i < 16; i++){
+  for(int i = 0; i < 8; i++){
     digitalWrite(sr.serial, array[i]);
     digitalWrite(sr.SRCLK, HIGH);
     digitalWrite(sr.SRCLK, LOW);
@@ -23,8 +23,8 @@ void writeRegister(struct shiftRegister sr, int array[16]){
 
 void clearRegister(struct shiftRegister sr){
   digitalWrite(sr.outputEnable, HIGH);
-  digitalWrite(sr.clear, HIGH);
   digitalWrite(sr.clear, LOW);
+  digitalWrite(sr.clear, HIGH);
   digitalWrite(sr.serial, LOW);
   digitalWrite(sr.outputEnable, LOW);
 }
@@ -40,85 +40,22 @@ void inverseArray(int *array[8]){
   }
 }
 
-void writeDisplay(int array[8][8]){
-  int rowon[16];
-  int rowoff[16];
-  int done[16] = {0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1};
-  int invdone[16] = {1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0};
-  int colon[16];
-  int coloff[16];
-  
-  for(int j = 0; j < 8; j++){
-      rowon[j] = 0;
-      rowoff[j+8] = 1;
-      colon[j+8] = 1;
-      coloff[j] = 0;
-  }
-  for(int i = 0; i < 8; i++){
-    for(int j = 0; j < 8; j++){
-      rowoff[j] = array[j][i];
-      if (array[j][i]){
-        rowon[j+8] = 0;
-      } else {
-        rowon[j+8] = 1;
-      }
-      colon[j] = 0;
-      coloff[j+8] = 1;
-    }
-    colon[i] = 1;
-    coloff[i+8] = 0;
 
-    writeRegister(col, colon);
-    writeRegister(row, rowon);
-    delay(100);
-    writeRegister(col, coloff);
-    writeRegister(row, rowoff);
-    delay(100);
-    writeRegister(col, done);
-    writeRegister(row, done);
-
-    Serial.print("\nCOLUMN: ");
-    Serial.print(i);    
-    Serial.print(" WROTE:\nCOLON: ");
-    for(int i = 0; i < 16; i++){Serial.print(colon[i]);}
-    Serial.print("\nROWON: ");
-    for(int i = 0; i < 16; i++){Serial.print(rowon[i]);}
-    Serial.print("\nCOLOFF:");
-    for(int i = 0; i < 16; i++){Serial.print(coloff[i]);}
-    Serial.print("\nROWOFF:");
-    for(int i = 0; i < 16; i++){Serial.print(rowoff[i]);}
-    Serial.print("\nDONE:  ");
-    for(int i = 0; i < 16; i++){Serial.print(done[i]);}
-    Serial.print("\nEND WRITE");
-  }
-}
 void setup() {
-  int done[16] = {0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1};
   Serial.begin(9600);
   col.serial = 2;
   col.RCLK = 3;
   col.SRCLK = 4;
   col.outputEnable = 5;
   col.clear = 6;
-  row.serial = 7;
-  row.RCLK = 8;
-  row.SRCLK = 9;
-  row.outputEnable = 10;
-  row.clear = 11;
   pinMode(col.serial, OUTPUT);
   pinMode(col.outputEnable, OUTPUT);
   pinMode(col.RCLK, OUTPUT);
   pinMode(col.SRCLK, OUTPUT);
   pinMode(col.clear, OUTPUT);
-  pinMode(row.serial, OUTPUT);
-  pinMode(row.outputEnable, OUTPUT);
-  pinMode(row.RCLK, OUTPUT);
-  pinMode(row.SRCLK, OUTPUT);
-  pinMode(row.clear, OUTPUT);
   digitalWrite(col.outputEnable, LOW);
+  digitalWrite(col.clear, HIGH);
   digitalWrite(col.clear, LOW);
-  digitalWrite(row.outputEnable, LOW);
-  digitalWrite(row.clear, LOW);
 }
 
 void loop() {
@@ -132,8 +69,20 @@ void loop() {
   writeRegister(row, testarr);
   delay(1000);
   */
-  
+  int coloff[8] = {0,0,0,0,0,0,0,1};
+  int colon[8] = {1,1,1,1,0,0,1,0};
+  int colt1[8] = {0,0,0,0,0,1,0,0};
+  int colt2[8] = {1,1,1,1,1,0,0,0};
 
+  writeRegister(col, coloff);
+  delay(1000);
+  writeRegister(col, colon);
+  delay(1000);
+  writeRegister(col, colt1);
+  delay(1000);
+  writeRegister(col, colt2);
+  delay(1000);
+  /*
   int circle[8][8] = {{0,0,0,0,0,0,0,0},{0,0,0,1,1,0,0,0},{0,0,1,0,0,1,0,0},{0,1,0,0,0,0,1,0},{0,1,0,0,0,0,1,0},{0,0,1,0,0,1,0,0},{0,0,0,1,1,0,0,0},{0,0,0,0,0,0,0,0}};
   int blank[8][8] = {{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0}};
   int full[8][8] = {{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1}};
@@ -147,5 +96,6 @@ void loop() {
   delay(1000);
   writeDisplay(full);
   delay(1000);
+  */
 
 }
